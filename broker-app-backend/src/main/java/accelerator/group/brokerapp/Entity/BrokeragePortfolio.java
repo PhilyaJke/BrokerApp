f9304a699f7cefff6c5c8343c2247eccf6b8e34e
PhilyaJke <borozdinfilipp@gmail.com>
2023-05-03 19:10:37 +0300

    fix

--- broker-app-backend/src/main/java/accelerator/group/brokerapp/Entity/BrokeragePortfolio.java
+++ broker-app-backend/src/main/java/accelerator/group/brokerapp/Entity/BrokeragePortfolio.java
@@ -9,8 +9,33 @@ import org.hibernate.annotations.GenericGenerator;
 import javax.persistence.*;
 import java.util.UUID;
 
+@NamedNativeQueries({
+        @NamedNativeQuery(
+                name = "FindUserssSecurities",
+                query = "SELECT s.region, s.figi, s.name, s.ticker, s.sector, s.icon_path FROM securities s" +
+                        " WHERE s.id IN (SELECT bps.securities_id FROM brokerage_portfolio_securities bps" +
+                        " INNER JOIN brokerage_portfolio bp on bp.id = bps.brokerage_portfolio_id" +
+                        " WHERE (cast(bp.user_id as varchar(255)) = cast(?1 as varchar(255))))",
+                resultSetMapping = "Mapping.FindUsersSecurities"
+        )
+})
 
 
+@SqlResultSetMappings({
+
+        @SqlResultSetMapping(name="Mapping.FindUserssSecurities",
+                classes = { @ConstructorResult(targetClass = Securities.class,
+                        columns = {
+                                @ColumnResult(name = "region", type = String.class),
+                                @ColumnResult(name = "figi", type = String.class),
+                                @ColumnResult(name = "price", type = Double.class),
+                                @ColumnResult(name = "name", type = String.class),
+                                @ColumnResult(name = "ticker", type = String.class),
+                                @ColumnResult(name = "sector", type = String.class),
+                                @ColumnResult(name = "icon_path", type = String.class)
+                        })
+                })
+})
 
 @Entity
 @Data
